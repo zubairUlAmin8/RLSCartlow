e28890f9f08b0cd6bd69cbac19adb20042ea5eb5
zubair <zubairulamin8@gmail.com>
2022-08-23 14:24:31 +0500

    new change

--- src/main/java/utlis/DriverSetup.java
+++ src/main/java/utlis/DriverSetup.java
@@ -1,4 +1,16 @@
 package utlis;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class DriverSetup {
+    private static WebDriver driver;
+    private DriverSetup(){
+    }
+    public static WebDriver getDriver(){
+        if(driver==null){
+            driver= new ChromeDriver();
+        }
+    return driver;
+    }
 }
