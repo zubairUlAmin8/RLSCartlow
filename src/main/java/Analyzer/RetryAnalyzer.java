d8471160fbc0c6d7aebaae4c6a4e58ce026854ee
zubair ul amin <zubairulamin8@gmail.com>
2022-08-28 14:17:56 +0500

    my analyzer added

--- src/main/java/Analyzer/RetryAnalyzer.java
+++ src/main/java/Analyzer/RetryAnalyzer.java
@@ -3,11 +3,12 @@ package Analyzer;
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 
+
 public class RetryAnalyzer implements IRetryAnalyzer{
 
 	
 	int counter = 0;
-	int retryLimit = 3;
+	int retryLimit = 2;
 	
 	
 	
--- src/test/java/base_test/BaseTest.java
+++ src/test/java/base_test/BaseTest.java
@@ -1,16 +1,26 @@
 package base_test;
 
+import Analyzer.RetryAnalyzer;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import utlis.DriverSetup;
 
 public class BaseTest {
+    SoftAssert soft;
     WebDriver driver;
 
-    @Test
+    @Test(retryAnalyzer = RetryAnalyzer.class)
     private void openWebsite(){
         driver=DriverSetup.getDriver();
         driver.get("http://web.cartlow.net/");
+//        soft.assertEquals("true", "false");
+    }
+    @Test
+    private void myprint(){
+//        soft.assertEquals("true", "true");
+
+
     }
 }
