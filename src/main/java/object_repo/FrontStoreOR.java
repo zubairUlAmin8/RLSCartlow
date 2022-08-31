1156fef0e82d15473011a6cce98068e9968bb1bb
zubair <zubairulamin8@gmail.com>
2022-08-31 18:48:51 +0500

    cartlow front store

---
+++ src/main/java/object_repo/FrontStoreOR.java
@@ -0,0 +1,9 @@
+package object_repo;
+
+public class FrontStoreOR {
+    public static final String addToCartButton="//div//button[@class='btn btn-primary  btn-block add_cart_button']"; //xpath
+    public static final String viewCartItems="//*[@id=\"cart-updater\"]/div/div[1]/div[2]/a[1]"; //xpath
+    public static final String cartIcon="//*[@id=\"cart-updater\"]/a"; //xpath
+
+    public  static  final String checkOutButton= "//*[@id=\"cart-container\"]/div/div/aside/div[1]/div/a";
+}
